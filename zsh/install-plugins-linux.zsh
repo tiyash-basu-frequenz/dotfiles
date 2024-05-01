d939a26f54cbe6ce656d8958d9e4590a682759ce
Tiyash Basu <tiyashbasu@gmail.com>
2024-05-01 14:32:43 +0200

    [zsh] Add script to setup zsh plugins

---
+++ zsh/install-plugins-linux.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+dnf config-manager --add-repo https://download.opensuse.org/repositories/shells:zsh-users:zsh-history-substring-search/Fedora_Rawhide/shells:zsh-users:zsh-history-substring-search.repo
+
+dnf update
+
+dnf install starship zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search
