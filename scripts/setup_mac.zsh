debe21e52bfb1f4ac54208c2230e234de2c83d3b
Tiyash Basu <tiyashbasu@gmail.com>
2024-07-14 15:01:30 +0200

    Add a util script to install tools in MacOS

---
+++ scripts/setup_mac.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+brew install \
+    btop \
+    eza \
+    fastfetch \
+    font-fira-code-nerd-font \
+    gnupg \
+    jq \
+    neovim \
+    podman \
+    rust \
+    starship \
+    tmux \
+    visual-studio-code \
+    zsh-autosuggestions \
+    zsh-history-substring-search \
+    zsh-syntax-highlighting
