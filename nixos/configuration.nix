03531a4a5827dc76fdf3a25a71c99af1d023d8a7
Tiyash Basu <tiyashbasu@gmail.com>
2024-07-14 20:38:31 +0200

    [nixos] Enable container support

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -141,7 +141,7 @@
     nano
     neovim
     nodejs
-    podman
+    podman-tui
     python3Full
     rustup
     starship
@@ -183,6 +183,20 @@
   #   options = [ "defaults" "nofail" ];
   # };
 
+  # Enable common container config files in /etc/containers
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -191,3 +205,4 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 }
+
