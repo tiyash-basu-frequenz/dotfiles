217fd2fc410732eb9ef3ec9c1fb63098a4ee48d5
Tiyash Basu <tiyashbasu@gmail.com>
2024-09-23 13:15:55 +0200

    [nvim] Disable format-on-save for a few file types

--- nvim/lua/local/on-save-format.lua
+++ nvim/lua/local/on-save-format.lua
@@ -1,8 +1,26 @@
+-- List of filetypes where you want to disable auto-formatting
+local excluded_filetypes = { "json", "markdown", "txt" }
+
 vim.api.nvim_create_augroup("AutoFormatting", {})
+
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = { "*" },
-    group = "AutoFormatting",
+group = "AutoFormatting",
     callback = function()
-        vim.lsp.buf.format()
+        -- Get the current buffer's filetype
+        local filetype = vim.bo.filetype
+
+        -- Check if the current file's filetype is in the excluded list
+        if vim.tbl_contains(excluded_filetypes, filetype) then
+            return -- Skip formatting for this file type
+        end
+
+        -- Proceed with formatting for other file types
+        vim.lsp.buf.format({
+            async = true,
+            filter = function(client)
+                return client.supports_method("textDocument/formatting")
+            end,
+        })
     end,
 })
