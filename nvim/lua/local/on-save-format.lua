aee0c96213dc9b24054703dcf10e2ac8cffdcdb3
Tiyash Basu <tiyashbasu@gmail.com>
2024-09-23 13:58:35 +0200

    [nvim] Add exception rules for makefiles

--- nvim/lua/local/on-save-format.lua
+++ nvim/lua/local/on-save-format.lua
@@ -1,6 +1,17 @@
 -- List of filetypes where you want to disable auto-formatting
 local excluded_filetypes = { "json", "markdown", "txt" }
 
+-- Table of specific settings for filetypes
+local filetype_settings = {
+    make = {
+        expandtab = false, -- Use hard tabs for Makefiles
+        tabstop = 4,       -- A tab character is 8 spaces wide
+        shiftwidth = 4,    -- Indentation is 8 spaces wide
+    },
+    -- You can add more filetypes here if needed, e.g.
+    -- lua = { expandtab = true, tabstop = 4, shiftwidth = 4 }
+}
+
 vim.api.nvim_create_augroup("AutoFormatting", {})
 
 vim.api.nvim_create_autocmd("BufWritePre", {
@@ -15,6 +26,14 @@ vim.api.nvim_create_autocmd("BufWritePre", {
             return -- Skip formatting for this file type
         end
 
+        -- Apply filetype-specific settings if they exist
+        if filetype_settings[filetype] then
+            local settings = filetype_settings[filetype]
+            vim.bo.expandtab = settings.expandtab
+            vim.bo.tabstop = settings.tabstop
+            vim.bo.shiftwidth = settings.shiftwidth
+        end
+
         -- Proceed with formatting for other file types
         vim.lsp.buf.format({
             async = true,
@@ -24,3 +43,4 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         })
     end,
 })
+
