e4a3766ca79a7c8336ce4d968e1e6abf58bee306
Tiyash Basu <tiyashbasu@gmail.com>
2024-09-24 12:13:57 +0200

    [nvim] Add shortcut descriptions

--- nvim/lua/local/vim-keymaps.lua
+++ nvim/lua/local/vim-keymaps.lua
@@ -1,31 +1,31 @@
 -- quit
-vim.keymap.set("n", "<C-q>", ":q<CR>", {})
+vim.keymap.set("n", "<C-q>", ":q<CR>", { desc = "Quit" })
 
 -- save
-vim.keymap.set("n", "<C-s>", ":w<CR>", {})
+vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save" })
 vim.keymap.set("i", "<C-s>", function()
     vim.api.nvim_command("write")
     vim.api.nvim_command("startinsert")
 end, {})
 
 -- shortcuts - neotree
-vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", {})
+vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", { desc = "Open file browser" })
 
 -- shortcuts - telescope
 -- see https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#pickers
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
-vim.keymap.set("n", "<leader>fj", builtin.jumplist, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics" })
+vim.keymap.set("n", "<leader>fj", builtin.jumplist, { desc = "Jump list" })
 
 -- shortcuts - LSP
 -- https://neovim.io/doc/user/lsp.html
-vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, {})
-vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
-vim.keymap.set("n", "<leader>cr", vim.lsp.buf.references, {})
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, { desc = "Show hover information" })
+vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "<leader>cr", vim.lsp.buf.references, { desc = "Show references" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Show code actions" })
+vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format code" })
 vim.keymap.set("n", "<leader>e", function()
     vim.diagnostic.open_float({ scope = "line" })
 end, { desc = "Show diagnostics for current line" })
@@ -40,16 +40,10 @@ vim.keymap.set("n", "<C-k>", function()
 end, { desc = "Toggle Inlay Hints" })
 
 -- shortcuts - git
-vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview git hunk" })
 
 -- shortcuts - refactor
 local refactor = require("refactor")
-vim.keymap.set("n", "<F3>", function()
-    refactor.text_replace_word()
-end, {})
-vim.keymap.set("v", "<F3>", function()
-    refactor.text_replace_selection()
-end, {})
-vim.keymap.set("n", "<F2>", function()
-    refactor.lsp_rename_symbol()
-end, {})
+vim.keymap.set("n", "<F3>", refactor.text_replace_word, { desc = "Replace word" })
+vim.keymap.set("v", "<F3>", refactor.text_replace_selection, { desc = "Replace selection" })
+vim.keymap.set("n", "<F2>", refactor.lsp_rename_symbol, { desc = "Rename symbol" })
