e30223cd0dd2c3bbe017c95a12d0f6383e94d532
Tiyash Basu <tiyashbasu@gmail.com>
2024-11-03 10:25:12 +0100

    [nixos] Reorganize config file

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 let
@@ -12,83 +8,86 @@ let
   unstable = import <nixos-unstable> {};
 in
 {
+  # ----------------------------------------------------------------------------
+  # Imports
+  # ----------------------------------------------------------------------------
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Enable OpenGL
-  hardware.opengl.enable = true;
+  # ----------------------------------------------------------------------------
+  # Additional Hardware Configuration
+  # ----------------------------------------------------------------------------
+  hardware.opengl.enable = true; # Enable OpenGL
 
-  # Bootloader.
+  # ----------------------------------------------------------------------------
+  # Bootloader Configuration
+  # ----------------------------------------------------------------------------
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  networking.hostFiles = [
-    (pkgs.writeTextFile {
-      name = "etc_hosts";
-      text = builtins.readFile /home/tiyash/etc_hosts;
-    })
-  ];
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # ----------------------------------------------------------------------------
+  # Basic NixOS Configuration
+  # ----------------------------------------------------------------------------
+  nixpkgs.config.allowUnfree = true; # Allow unfree packages
+  system.stateVersion = "24.05"; # NixOS release version
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
+  # ----------------------------------------------------------------------------
+  # Locale Settings
+  # ----------------------------------------------------------------------------
+  time.timeZone = "Europe/Berlin"; # Set your time zone.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
     LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
+    LC_MONETARY = "en_IE.UTF-8";
     LC_NAME = "de_DE.UTF-8";
     LC_NUMERIC = "en_US.UTF-8";
     LC_PAPER = "de_DE.UTF-8";
     LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
+    LC_TIME = "en_IE.UTF-8";
   };
 
-  # Enable Wayland for SDDM
-  services.displayManager.sddm.wayland.enable = true;
+  # ----------------------------------------------------------------------------
+  # Shell Configuration
+  # ----------------------------------------------------------------------------
+  programs.zsh.enable = true; # Enable Zsh shell
+  users.defaultUserShell = pkgs.zsh; # Set Zsh as the default shell
 
-  # Enable SDDM
-  services.displayManager.sddm.enable = true;
+  # ----------------------------------------------------------------------------
+  # Desktop Environment and Display Manager
+  # ----------------------------------------------------------------------------
+  # X11 configuration - Disable X11 server
+  services.xserver.enable = false;
 
-  # Enable KDE Plasma 6
+  # Enable SDDM with Wayland
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+
+  # Enable Plasma Desktop Environment
   services.desktopManager.plasma6.enable = true;
-  # Excluding a few KDE defaults
   environment.plasma6.excludePackages = with pkgs.libsForQt5; [
     elisa
     kate
     konsole
     oxygen
     plasma-browser-integration
-    spectacle
   ];
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = false;
+  # Enable Hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 
-  # Configure keymap in X11
-  services.xserver.xkb.layout = "us";
-  services.xserver.xkb.variant = "";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  # ----------------------------------------------------------------------------
+  # Sound Management Configuration with Pipewire
+  # ----------------------------------------------------------------------------
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -96,80 +95,33 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Install Hyprland
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
-  # Install zsh
-  programs.zsh.enable = true;
-
-  # Install neovim
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.tiyash = {
-    isNormalUser = true;
-    description = "Tiyash Basu";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      btop
-      eza
-      fastfetch
-      gh
-      grpcurl
-      jq
-      nodejs
-      protobuf
-      puppet-bolt
-      rustup
-      starship
-      tmux
-      zsh-autosuggestions
-      zsh-syntax-highlighting
-      zsh-history-substring-search
-      ## neovim plugin
-      black
-      fzf
-      lua-language-server
-      nodePackages.prettier
-      ripgrep
-      stylua
+  # ----------------------------------------------------------------------------
+  # Networking Configuration
+  # ----------------------------------------------------------------------------
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+    hostFiles = [
+      (pkgs.writeTextFile {
+        name = "etc_hosts";
+        text = builtins.readFile /home/tiyash/etc_hosts;
+      })
     ];
   };
 
-  # Set default user shell to zsh
-  users.defaultUserShell = pkgs.zsh;
+  # Enable SSH
+  services.openssh.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # ----------------------------------------------------------------------------
+  # Printing Services
+  # ----------------------------------------------------------------------------
+  services.printing.enable = true;
+
+  # ----------------------------------------------------------------------------
+  # System Packages
+  # ----------------------------------------------------------------------------
   environment.systemPackages = with pkgs; [
     alacritty
     chromium
@@ -190,8 +142,7 @@ in
     unzip
     usbutils
     wl-clipboard
-
-    ## Hyprland packages
+    # Hyprland packages
     hyprcursor
     hypridle
     hyprlock
@@ -203,55 +154,68 @@ in
     waybar
     wofi
     xcur2png
-
-    ## If this is in a UTM VM:
-    # spice-vdagent
-    # virtiofsd
   ];
 
-  # Start chromium with wayland support
+  # Install neovim
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
   nixpkgs.config.chromium = {
     commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
   };
 
-  # List services that you want to enable:
+  # Add any packages you need from the unstable channel here if necessary.
+  # E.g., to install a package from the unstable channel:
+  # environment.systemPackages = with pkgs; [
+  #   unstable.packageName
+  # ];
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Uncoment If this system is a UTM AppleVF VM
-  # This makes the shared directory available to the VM at `/mnt/shared`
-  # fileSystems."/mnt/shared" = {
-  #   fsType = "virtiofs";
-  #   device = "share";
-  #   options = [ "defaults" "nofail" ];
-  # };
-
-  # Enable common container config files in /etc/containers
+  # ----------------------------------------------------------------------------
+  # Podman Configuration
+  # ----------------------------------------------------------------------------
   virtualisation.containers.enable = true;
-  virtualisation = {
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true; # Create a `docker` alias for Podman
+    defaultNetwork.settings.dns_enabled = true;
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+
+  # ----------------------------------------------------------------------------
+  # User Configuration
+  # ----------------------------------------------------------------------------
+  users.users.tiyash = {
+    isNormalUser = true;
+    description = "Tiyash Basu";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      btop
+      eza
+      fastfetch
+      gh
+      grpcurl
+      jq
+      nodejs
+      protobuf
+      puppet-bolt
+      rustup
+      # Terminal plugins
+      starship
+      tmux
+      zsh-autosuggestions
+      zsh-syntax-highlighting
+      zsh-history-substring-search
+      # Neovim plugins
+      black
+      fzf
+      lua-language-server
+      nodePackages.prettier
+      ripgrep
+      stylua
+    ];
+  };
+
 }
