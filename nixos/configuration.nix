0e5e13e046f993f5f2bee6590a75a5dfda30ac58
Tiyash Basu <tiyashbasu@gmail.com>
2024-11-03 17:33:44 +0100

    [nixos] Update locale

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -41,11 +41,11 @@ in
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "en_IE.UTF-8";
+    LC_MEASUREMENT = "en_IE.UTF-8";
     LC_MONETARY = "en_IE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
+    LC_NAME = "en_IE.UTF-8";
+    LC_NUMERIC = "en_IE.UTF-8";
     LC_PAPER = "de_DE.UTF-8";
     LC_TELEPHONE = "de_DE.UTF-8";
     LC_TIME = "en_IE.UTF-8";
