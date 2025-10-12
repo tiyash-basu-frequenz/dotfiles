65795ab43204a599b242a1de8a01d34b5162552d
Tiyash Basu <tiyashbasu@gmail.com>
2025-10-12 14:43:27 +0200

    [fish] Set npm's target directory manually

--- fish/config.fish
+++ fish/config.fish
@@ -9,4 +9,10 @@ end
 fish_add_path "$HOME/Dev/fqz/dev_containers/rust_arm64_host/scripts"
 fish_add_path "$HOME/Dev/fqz/dev_containers/go/scripts"
 fish_add_path "$HOME/Dev/fqz/dev_containers/python/scripts"
+
+# Rust
 fish_add_path "$HOME/.rustup/toolchains/stable-aarch64-unknown-linux-gnu/bin"
+
+# NPM global packages
+set -gx NPM_CONFIG_PREFIX ~/.npm-global
+fish_add_path "$HOME/.npm-global/bin"
