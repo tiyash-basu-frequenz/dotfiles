de1f24ac292a6a458df695619e39fd6c6fb76493
Tiyash Basu <tiyash.basu@frequenz.com>
2025-11-30 14:25:25 +0100

    [nvim] Sync config
    Signed-off-by: Tiyash Basu <tiyash.basu@frequenz.com>

--- nvim/lua/local/vim-keymaps.lua
+++ nvim/lua/local/vim-keymaps.lua
@@ -58,7 +58,7 @@ vim.keymap.set("n", "<F5>", refactor.lsp_rename_symbol, { desc = "Rename symbol"
 
 -- shortcuts - Copilot
 vim.g.copilot_no_tab_map = true
-vim.keymap.set("i", "<C-CR>", "copilot#Accept(\"\\<CR>\")", {
+vim.keymap.set("i", "<C-Space>", "copilot#Accept(\"\\<CR>\")", {
     expr = true,
     replace_keycodes = false,
     desc = "Accept Copilot suggestion",
